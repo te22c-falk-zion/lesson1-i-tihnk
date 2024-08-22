5d7b01a46ff57456389c2dc71760da66bf4d2d7c
Zion Falk <zion.falk@elev.ga.ntig.se>
2024-08-22 16:08:06 +0200

    hangman commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+string word = "gooning";
+
+string[] hiddenWord = {"_", "_","_","_","_","_","_"};
+
+Console.WriteLine("hangman...");
+
+foreach (string letter in hiddenWord)
+{
+    Console.Write(letter);
+}
+
+Console.WriteLine("\nletter...now");
+
+Console.ReadLine();
